b8f8eadf90cc772f7278ad2ada1aace0dafac8fa
rifkegribenes <rifkegribenes@gmail.com>
2019-02-10 14:10:49 -0800

    remove files used for remote hosting

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-import os
-
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
+from db import db
 from security import authenticate, identity
 from resources.user import UserRegister, User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'fawe8jyhnklj;mja8wi2;'
+app.secret_key = 'af7nuhdgp8asagphpua'
 api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
 jwt = JWT(app, authenticate, identity)  # /auth
@@ -27,6 +31,5 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 
 if __name__ == '__main__':
-  from db import db
-  db.init_app(app)
-  app.run(port=5000, debug=True)
+    db.init_app(app)
+    app.run(port=5000, debug=True)
