191389367df3713e4e4a34135392b87c933465af
rifkegribenes <rifkegribenes@gmail.com>
2019-02-10 20:30:25 -0800

    add jwt claims

--- app.py
+++ app.py
@@ -22,6 +22,13 @@ def create_tables():
 
 jwt = JWTManager(app)
 
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+	if identity == 1:
+		return {'is_admin': True}
+	return {'is_admin': False}
+
+
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
