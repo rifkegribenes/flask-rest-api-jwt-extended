fc0d45fd8498933375bb401ac71e3d41c22b6c9d
rifkegribenes <rifkegribenes@gmail.com>
2019-02-11 22:02:03 -0800

    blacklisting w jwt-extended

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
@@ -6,11 +6,14 @@ from db import db
 from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.secret_key = 'af7nuhdgp8asagphpua'
 api = Api(app)
 
@@ -28,7 +31,12 @@ def add_claims_to_jwt(identity):
 		return {'is_admin': True}
 	return {'is_admin': False}
 
-@jwt.expire_token_loader
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+	return decrypted_token['identity'] in BLACKLIST
+
+@jwt.expired_token_loader
 def expired_token_callback():
 	return jsonify({
 		'description': 'The token has expired.',
